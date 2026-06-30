21ad2ff595de022de377d8406d9d853e72eb3885
l.braud <l.braud@student.isartdigital.com>
2026-06-30 12:00:14 +0200

    feat: wip leader action

---
+++ Assets/Scripts/Squad/LeaderActions/ExploreLeaderAction.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ExploreLeaderAction : LeaderAction
+{
+    public Vector3 exploreDest;
+    public float exploreRadius = 30f;
+
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+}
